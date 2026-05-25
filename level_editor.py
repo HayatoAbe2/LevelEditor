67e6d37d9f5f42a7481d80bfbec8862407713927
HayatoAbe2 <k024g0033@g.neec.ac.jp>
2026-05-25 18:24:14 +0900

    立方体でオブジェクトのコライダーを表示

--- level_editor.py
+++ level_editor.py
@@ -1,6 +1,10 @@
 import bpy
 import bpy_extras
 import math
+import gpu
+import gpu_extras.batch
+import copy
+
 
 bl_info = {
     "name": "レベルエディタ",
@@ -12,6 +16,7 @@ bl_info = {
     "warning": "",
     "category": "Object",
 }
+
 
 # メニュー
 def draw_menu_manual(self, context):
@@ -27,8 +32,8 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     def draw(self, context):
         # 項目追加
         self.layout.operator("wm.url_open_preset", text= "Manual", icon= 'HELP')
-        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label) 
-        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label) 
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
         self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
 
     # 既存のメニューにサブメニューを追加
@@ -61,7 +66,7 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
         bpy.ops.mesh.primitive_ico_sphere_add()
         print("ICO球を生成しました。")
         return {'FINISHED'}
-    
+   
 # オペレータ(シーン出力)
 class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
     bl_idname = "myaddon.myaddon_ot_export_scene"
@@ -80,7 +85,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         self.report({'INFO'}, "シーン情報をExportしました。")
 
         return {'FINISHED'}
-    
+   
     def export(self):
         """ファイルに出力"""
 
@@ -109,7 +114,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
     def parse_scene_recursive(self, file, object, level):
         """シーン解析用再帰関数"""
 
-        # 深さ分インデント
+        # 深さ分けインデント
         indent = ''
         for i in range(level):
             indent += "\t"
@@ -172,6 +177,79 @@ class MYADDON_OT_add_filename(bpy.types.Operator):
 
         return {"FINISHED"}
 
+# コライダー描画
+class DrawCollider:
+    handle = None
+
+    # 描画関数
+    def draw_collider():
+        # 頂点
+        vertices = {"pos":[]}
+
+        # インデックス
+        indices = []
+
+        # 頂点オフセット
+        offsets = [
+        [-0.5,-0.5,-0.5],
+        [+0.5,-0.5,-0.5],
+        [-0.5,+0.5,-0.5],
+        [+0.5,+0.5,-0.5],
+        [-0.5,-0.5,+0.5],
+        [+0.5,-0.5,+0.5],
+        [-0.5,+0.5,+0.5],
+        [+0.5,+0.5,+0.5],
+        ]
+        # 立方体サイズ
+        size =[2,2,2]
+
+        # シーンのオブジェクト
+        for object in bpy.context.scene.objects:
+            # 追加前の頂点登録数
+            start = len(vertices["pos"])
+
+            # 各頂点
+            for offset in offsets:
+                # 中心座標をコピー
+                pos = copy.copy(object.location)
+                pos[0]+=offset[0]*size[0]
+                pos[1]+=offset[1]*size[1]
+                pos[2]+=offset[2]*size[2]
+                
+                # 頂点データリストに追加
+                vertices['pos'].append(pos)
+
+                # インデックスデータ追加
+                # 前
+                indices.append([start+0, start+1])
+                indices.append([start+2, start+3])
+                indices.append([start+0, start+2])
+                indices.append([start+1, start+3])
+                # 奥
+                indices.append([start+4, start+5])
+                indices.append([start+6, start+7])
+                indices.append([start+4, start+6])
+                indices.append([start+5, start+7])
+                # 横
+                indices.append([start+0, start+4])
+                indices.append([start+1, start+5])
+                indices.append([start+2, start+6])
+                indices.append([start+3, start+7])
+
+        # シェーダ
+        shader = gpu.shader.from_builtin("UNIFORM_COLOR")
+
+        # バッチ作成
+        batch = gpu_extras.batch.batch_for_shader(shader,"LINES", vertices, indices = indices)
+
+        # シェーダのパラメータ
+        color = [0.5, 1.0, 1.0, 1.0]
+        shader.bind()
+        shader.uniform_float("color", color)
+
+        # 描画
+        batch.draw(shader)
+
 # Blenderに登録するクラスリスト
 classes = (
     MYADDON_OT_stretch_vertex,
@@ -182,6 +260,7 @@ classes = (
     OBJECT_PT_file_name,
 )
 
+
 # 有効化時
 def register():
     # Blenderにクラスを登録
@@ -190,6 +269,9 @@ def register():
 
     # メニューに項目を追加
     bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.subMenu)
+    # 3Dビューに描画関数を追加
+    DrawCollider.handle = bpy.types.SpaceView3D.draw_handler_add(DrawCollider.draw_collider, (),"WINDOW","POST_VIEW")
+    
     print("レベルエディタ:有効")
 
 # 無効化時
@@ -199,9 +281,13 @@ def unregister():
     # Blenderからクラスを削除
     for cls in classes:
         bpy.utils.unregister_class(cls)
+    # 3Dビューから描画関数を削除
+    bpy.types.SpaceView3D.draw_handler_remove(DrawCollider.handle, "WINDOW")
 
     print("レベルエディタ:無効")
+
 
 # テスト
 if __name__ == "__main__":
     register()
+
